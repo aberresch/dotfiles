eac4941467157c85baf9a2cac4ed40a68e171caa
pustina <pustina@cs.uni-bonn.de>
2015-03-22 16:50:01 +0100

    Added a few more Vim plugins

--- install.sh
+++ install.sh
@@ -2,7 +2,18 @@
 FILES="aliases ansible.cfg dl_centerdevice.zsh screenrc vimrc.before vimrc.after zshrc"
 MKDIRS="${HOME}/.janus"
 DIRS="janus/ftdetect janus/lukas"
-GITREPOS="https://github.com/lukaspustina/oh-my-zsh.git,${HOME}/.oh-my-zsh https://github.com/carlhuda/janus.git,${HOME}/.vim,rake https://github.com/gavinbeatty/dragvisuals.vim.git,${HOME}/.janus/dragvisuals.vim.git https://github.com/chase/vim-ansible-yaml.git,${HOME}/.janus/vim-ansible-yaml.git https://github.com/altercation/vim-colors-solarized.git,${HOME}/.janus/vim-colors-solarized.git https://github.com/tpope/vim-fugitive.git,${HOME}/.janus/vim-fugitive.git https://github.com/atweiden/vim-vmath.git,${HOME}/.janus/vim-vmath.git"
+GITREPOS="\
+  https://github.com/lukaspustina/oh-my-zsh.git,${HOME}/.oh-my-zsh \
+  https://github.com/carlhuda/janus.git,${HOME}/.vim,rake \
+  https://github.com/gavinbeatty/dragvisuals.vim.git,${HOME}/.janus/dragvisuals.vim.git \
+  https://github.com/chase/vim-ansible-yaml.git,${HOME}/.janus/vim-ansible-yaml.git \
+  https://github.com/altercation/vim-colors-solarized.git,${HOME}/.janus/vim-colors-solarized.git \
+  https://github.com/tpope/vim-fugitive.git,${HOME}/.janus/vim-fugitive.git \
+  https://github.com/atweiden/vim-vmath.git,${HOME}/.janus/vim-vmath.git \
+  https://github.com/tpope/vim-surround.git,${HOME}/.janus/vim-surround.git \
+  https://github.com/tpope/vim-repeat.git,${HOME}/.janus/vim-repeat.git \
+  https://github.com/sjl/gundo.vim.git,${HOME}/.janus/gundo \
+"
 
 CWD=$(pwd)
 
